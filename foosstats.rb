7c4d0bb8e65b95f160da4bbabb3ba0d7b368ce36
Ryan Slade <ryanslade@gmail.com>
2010-03-21 12:48:22 +0000

    Datamapper set up correctly

--- foosstats.rb
+++ foosstats.rb
@@ -1,10 +1,6 @@
 require "rubygems"
 require "sinatra"
-require "datamapper"
 require "models"
-
-DataMapper.setup(:default, ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/stats.db")
-DataMapper.auto_migrate!
 
 get '/' do
   erb :index
--- models.rb
+++ models.rb
@@ -1,11 +1,14 @@
 require "datamapper"
 
+DataMapper.setup(:default, ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/stats.db")
+DataMapper.auto_migrate!
+
 class Player
   include DataMapper::Resource
   
-  property :id, Serial
+  property :id, Serial, :key => true 
   property :first_name, String
   property :last_name, String
   property :email, String
-  property :created_at, DateTime, :default => Proc.new { Time.now } 
+  property :created_at, DateTime, :default => Proc.new { Time.now }
 end
