fc26a9fcc54a1fda81ec47daa736f06a599b05fa
Ryan Slade <ryanslade@gmail.com>
2010-03-21 15:06:36 +0000

    Added a Game model Added basic player form

--- foosstats.rb
+++ foosstats.rb
@@ -2,7 +2,14 @@ require "rubygems"
 require "sinatra"
 require "models"
 
-get '/' do
+get "/" do
   erb :index
 end
 
+get "/users/new" do
+  erb :new_user
+end
+
+post "/add_user" do
+  Player.create(params)
+end
--- models.rb
+++ models.rb
@@ -5,10 +5,26 @@ DataMapper.auto_migrate!
 
 class Player
   include DataMapper::Resource
+
+  property :id, Serial, :key => true
+  property :created_at, DateTime, :default => Proc.new { Time.now }
   
-  property :id, Serial, :key => true 
   property :first_name, String
   property :last_name, String
-  property :email, String
+  property :email, String    
+end
+
+class Game
+  include DataMapper::Resource
+  
+  property :id, Serial, :key => true
   property :created_at, DateTime, :default => Proc.new { Time.now }
+  
+  property :team_one_attack, Integer
+  property :team_one_defense, Integer
+  property :team_one_score, Integer, :default => 0
+  
+  property :team_two_attack, Integer
+  property :team_two_defense, Integer
+  property :team_two_score, Integer, :default => 0
 end
