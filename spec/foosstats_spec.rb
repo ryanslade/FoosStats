bc1b584a66006a8c9b9e0618196525ba42fc8648
Ryan Slade <ryanslade@gmail.com>
2010-04-02 16:08:31 +0100

    Added an edge case player who should have no stats

--- spec/foosstats_spec.rb
+++ spec/foosstats_spec.rb
@@ -13,6 +13,9 @@ describe "Foos Stats" do
       Player.create(:first_name => "first#{n}", :last_name => "last#{n}", :email => "test#{n}@test.com")
     end
     
+    # Edge case player
+    Player.create(:first_name => "first", :last_name => "last", :email => "test@test.com")
+    
     8.times { Game.create(:team_one_attack => 1, :team_one_defense => 2, :team_two_attack => 3, :team_two_defense => 4, :team_one_score => 10, :team_two_score => 8) }
     5.times { Game.create(:team_one_attack => 1, :team_one_defense => 2, :team_two_attack => 3, :team_two_defense => 4, :team_one_score => 8, :team_two_score => 10) }
   end
