ce1c2e22f6a6f7603813c47c70ee993e6e0ef39b
Ryan Slade <ryanslade@gmail.com>
2010-04-02 13:40:11 +0100

    Added some test data

--- spec/foosstats_spec.rb
+++ spec/foosstats_spec.rb
@@ -7,6 +7,16 @@ describe "Foos Stats" do
     @app ||= Sinatra::Application
   end
 
+  before(:all) do
+    DataMapper.auto_migrate!
+    4.times do |n|
+      Player.create(:first_name => "first#{n}", :last_name => "last#{n}", :email => "test#{n}@test.com")
+    end
+    
+    10.times { Game.create(:team_one_attack => 1, :team_one_defense => 2, :team_two_attack => 3, :team_two_defense => 4, :team_one_score => 10, :team_two_score => 8) }
+    5.times { Game.create(:team_one_attack => 1, :team_one_defense => 2, :team_two_attack => 3, :team_two_defense => 4, :team_one_score => 8, :team_two_score => 10) }
+  end
+  
   it "should redirect to /games/recent when pointed at /" do
     get "/"
     follow_redirect!
